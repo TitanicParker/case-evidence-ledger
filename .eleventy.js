16215ecc562c4c72cae4c182a369ed4e37ae191f
TitanicParker <theliamo@gmail.com>
2026-08-16 16:45:39 +0100

    Make site build base-path aware

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
+const { basePath } = require("./site/_lib/paths.js");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"site/assets": "assets"});
   eleventyConfig.addPassthroughCopy("case-evidence-ledger_timeline.html");
   return {
-    dir: { input: "site", output: "_site", data: "_data" },
+    dir: { input: "site", output: process.env.SITE_OUTPUT_DIR || "_site", data: "_data" },
+    pathPrefix: basePath,
     htmlTemplateEngine: false,
     markdownTemplateEngine: false,
     templateFormats: ["11ty.js"]
